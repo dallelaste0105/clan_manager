a2ba62321cd01e23fa5801aef1c35d6ac87b2adf
dallalaste <gabrieldallelaste75@gmail.com>
2025-12-04 10:40:37 -0300

    fix: dependencies

--- frontend/clan_manager/src/App.tsx
+++ frontend/clan_manager/src/App.tsx
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<InsertScreen />} />
-
-      <Route path="/signup" element={<InsertScreen />} />
-
       <Route path="*" element={<Navigate to="/" replace />} />
+
+      <Route path="/insert" element={<InsertScreen />} />
+      
     </Routes>
   )
 }
