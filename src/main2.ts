a6a7089eb7c98d01965924dd2c4c295cf97fea64
dallalaste <gabrieldallelaste75@gmail.com>
2025-12-31 16:53:48 -0300

    feat: sprites

--- src/main2.ts
+++ src/main2.ts
@@ -8,63 +8,9 @@ import * as PIXI from "pixi.js";
     })
     document.body.appendChild(game.canvas);
 
-    const texture = await PIXI.Assets.load("/player.png");
+    const sheet = await PIXI.Assets.load('/player.json');
 
-    /* const runHorizontal = await Promise.all([
-    PIXI.Assets.load("/rh1.png"),
-    PIXI.Assets.load("/rh2.png"),
-    PIXI.Assets.load("/rh3.png"),
-    PIXI.Assets.load("/rh4.png"),
-    PIXI.Assets.load("/rh5.png"),
-    PIXI.Assets.load("/rh6.png"),
-    PIXI.Assets.load("/rh7.png"),
-    PIXI.Assets.load("/rh8.png"),
-    PIXI.Assets.load("/rh9.png"),
-    PIXI.Assets.load("/rh10.png"),
-    PIXI.Assets.load("/rh11.png"),
-    PIXI.Assets.load("/rh12.png"),
-    PIXI.Assets.load("/rh13.png"),
-    PIXI.Assets.load("/rh14.png"),
-    PIXI.Assets.load("/rh15.png"),
-    PIXI.Assets.load("/rh16.png"),
-    PIXI.Assets.load("/rh17.png"),
-    PIXI.Assets.load("/rh18.png"),
-    PIXI.Assets.load("/rh19.png"),
-    PIXI.Assets.load("/rh20.png"),
-    PIXI.Assets.load("/rh21.png"),
-    PIXI.Assets.load("/rh22.png"),
-    PIXI.Assets.load("/rh23.png"),
-    PIXI.Assets.load("/rh24.png"),
-]);
-
-    const moveVertical = await Promise.all([
-    PIXI.Assets.load("/rv1.png"),
-    PIXI.Assets.load("/rv2.png"),
-    PIXI.Assets.load("/rv3.png"),
-    PIXI.Assets.load("/rv4.png"),
-    PIXI.Assets.load("/rv5.png"),
-    PIXI.Assets.load("/rv6.png"),
-    PIXI.Assets.load("/rv7.png"),
-    PIXI.Assets.load("/rv8.png"),
-    PIXI.Assets.load("/rv9.png"),
-    PIXI.Assets.load("/rv10.png"),
-    PIXI.Assets.load("/rv11.png"),
-    PIXI.Assets.load("/rv12.png"),
-    PIXI.Assets.load("/rv13.png"),
-    PIXI.Assets.load("/rv14.png"),
-    PIXI.Assets.load("/rv15.png"),
-    PIXI.Assets.load("/rv16.png"),
-    PIXI.Assets.load("/rv17.png"),
-    PIXI.Assets.load("/rv18.png"),
-    PIXI.Assets.load("/rv19.png"),
-    PIXI.Assets.load("/rv20.png"),
-    PIXI.Assets.load("/rv21.png"),
-    PIXI.Assets.load("/rv22.png"),
-    PIXI.Assets.load("/rv23.png"),
-    PIXI.Assets.load("/rv24.png"),
-]); */
-
-    const player = new PIXI.AnimatedSprite(texture);
+    const player = new PIXI.AnimatedSprite(sheet.animations['down']);
 
     const blurFilter = new PIXI.BlurFilter();
     blurFilter.strength = 0;
@@ -78,7 +24,7 @@ import * as PIXI from "pixi.js";
     player.x = 500;
     player.y = 500;
     player.anchor.set(0.5);
-    const velocity = 5;
+    const velocity = 2;
     
     game.stage.addChild(player);
 
@@ -118,25 +64,49 @@ import * as PIXI from "pixi.js";
         }
     })
 
-    game.ticker.add((time)=>{
-        if (keys.a) {
-            player.x -= velocity;
-            blurFilter.strength = 10
-        }
-        if (keys.s) {
-            player.y += velocity;
-            blurFilter.strength = 10
-        }
-        if (keys.d) {
-            player.x += velocity;
-            blurFilter.strength = 10
-        }
+
+
+    game.ticker.add(() => {
+        let isMoving = false;
+
         if (keys.w) {
             player.y -= velocity;
-            blurFilter.strength = 10
+            isMoving = true;
+            if (player.textures !== sheet.animations['up']) {
+                player.textures = sheet.animations['up'];
+                player.play();
+            }
         }
-        if(keys.a == false && keys.s == false && keys.d == false && keys.w == false){
-            blurFilter.strength = 0
+        else if (keys.s) {
+            player.y += velocity;
+            isMoving = true;
+            if (player.textures !== sheet.animations['down']) {
+                player.textures = sheet.animations['down'];
+                player.play();
+            }
         }
-    })
+        else if (keys.a) {
+            player.x -= velocity;
+            isMoving = true;
+            if (player.textures !== sheet.animations['left']) {
+                player.textures = sheet.animations['left'];
+                player.play();
+            }
+        }
+        else if (keys.d) {
+            player.x += velocity;
+            isMoving = true;
+            if (player.textures !== sheet.animations['right']) {
+                player.textures = sheet.animations['right'];
+                player.play();
+            }
+        }
+
+        if (!isMoving) {
+            player.stop();
+            player.currentFrame = 0;
+        } else {
+            if (!player.playing) player.play();
+        }
+    });
 })();
